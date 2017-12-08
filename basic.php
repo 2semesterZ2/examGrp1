20ad243c473bfdf8a1ddd1c97cef6271adb5a86a
Petra Jakubcova <petrajakubcova@hotmail.com>
2017-12-08 13:40:34 +0100

    Change to php
    Basic.html to basic.php

---
+++ basic.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Botanical Park</title>
+		<!--CSS here-->
+	</head>
+
+	<body>
+		<!--Header:-->
+		<?php include("header.php"); ?>
+		<!--Navigation:-->
+		<?php include("navigation.php"); ?>
+			
+		<!--Content here-->
+		
+		<!--Footer:-->
+		<?php include("footer.php"); ?>
+	</body>
+
+</html>
