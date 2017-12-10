b496c7bcda5663dc67e3f935866617a466ac471d
Petra Jakubcova <petrajakubcova@hotmail.com>
2017-12-10 15:47:10 +0100

    Projects and events

--- blog.php
+++ blog.php
@@ -17,7 +17,7 @@
 			<h1>Blog title</h1>
 			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
 			
-			<!--Six xoxes with pictures and short description-->
+			<!--Six boxes with pictures and short description-->
 			
 			<div class="boxesblog">First box</div>
 			<!-- img src="" alt=""> -->
