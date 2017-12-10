aeb20a39431aec1ca7775c61b89f395e683d0354
Petra Jakubcova <petrajakubcova@hotmail.com>
2017-12-10 15:58:34 +0100

    Projects

---
+++ projects.php
@@ -0,0 +1,41 @@
+<!doctype html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Botanical Park</title>
+		<!--CSS here-->
+	</head>
+
+	<body>
+		<!--Header:-->
+		<?php include("header.php"); ?>
+		<!--Navigation:-->
+		<?php include("navigation.php"); ?>
+			
+		<!--Content here-->
+		<article>
+			<h1>Projects</h1>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+		
+				<h2>Playground project</h2>
+				<div class="galleryprojects">
+				<!-- Gallery of the pictures, one large and three small-->
+				</div>
+				<div class="descprojects">
+				<!-- Description of the project-->
+				</div>
+			
+				<h2>Other project</h2>
+				<div class="galleryprojects">
+				<!-- Gallery of the pictures, one large and three small-->
+				</div>
+				<div class="descprojects">
+				<!-- Description of the project-->
+				</div>
+				
+		</article>
+		<!--Footer:-->
+		<?php include("footer.php"); ?>
+	</body>
+
+</html>
