827517558665a9fb451493dc21bffa94f6630a44
UnkrautFee <kraftsa60869@th-nuernberg.de>
2017-12-11 12:57:19 +0100

    BugFix
    Wrong Id choosen, so linked to the Projects & Events instead of the Visitor Information

--- navigation.php
+++ navigation.php
@@ -94,7 +94,7 @@
         var btnEvents = document.getElementById("btnEventsAndProjects");
         btnEvents.addEventListener("click", function() {window.location.href = "projectsandevents.php";});
         
-        var btnVisitInfo = document.getElementById("btnEventsAndProjects");
+        var btnVisitInfo = document.getElementById("btnVisitInfo");
         btnVisitInfo.addEventListener("click", function() {window.location.href = "visitorinfo.php";});
         
     </script>
