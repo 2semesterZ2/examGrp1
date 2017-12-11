c1dd2b79f1fd9006261814225094092cb4abf378
Petra Jakubcova <petrajakubcova@hotmail.com>
2017-12-11 13:12:53 +0100

    change

--- visitorinfo.php
+++ visitorinfo.php
@@ -19,7 +19,7 @@
 		The park is open all day every day and is free to enter. It is a place where you can surround yourself with green lawns, a variety of interesting trees from its days as a botanical garden, as well as a small stream, with the entire area spanning 21.5 hectares.
 		The perfect place for a walk or jog, as well as picnics and barbecues in the warmer months. Dog walkers are also welcome, although dogs must be kept on leads at all times. Cycling is also only allowed along Peter Holms Vej at the top of the park.</p>
 		
-		<!--Four boxes with pictures and short description? or name on each of the pictures?-->
+		<!--Two boxes with pictures and short description? or name on each of the pictures?-->
 			
 			<div class="boxesvisitorinfo">Location</div>
 			<!-- img src="" alt=""> -->
@@ -33,6 +33,7 @@
 			
 			<!--In CSS div.boxesprjectsandevents img {} -->
 			<!--In CSS div.descprojectsandevents {} -->
+
 		</article>
 		<!--Footer:-->
 		<?php include("footer.php"); ?>
