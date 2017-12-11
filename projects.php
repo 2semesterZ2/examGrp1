a205cb57e74ea7e98edcc12cf225a9cda49f3e66
Petra Jakubcova <petrajakubcova@hotmail.com>
2017-12-11 13:35:39 +0100

    content

--- projects.php
+++ projects.php
@@ -14,24 +14,46 @@
 			
 		<!--Content here-->
 		<article>
-			<h1>Projects in the Botanical Park</h1>
-			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
 		
-				<h2>Playground project</h2>
-				<div class="galleryprojects">
-				<!-- Gallery of the pictures, one large and three small-->
-				</div>
-				<div class="descprojects">
-				<!-- Description of the project-->
-				</div>
+			<h1>Projects in the Botanical Park</h1>
+			<p>
+			<h2>Upcoming projects</h2>
+
+			<h3>New Playground in the Botanical Park</h3>
+			<p>(Insert picture here)
+			We are excited to announce that a new all-inclusive playground will be coming soon to the Botanical Park! With a variety of different structures and sensory experiences, the playground will be a fun place for children of all ages to interact with each other out in nature.
+			We want this to be a place that brings the community together, and where better to do this than in the heart of the Botanical Park. Meetings are already underway about getting this community-lead project started, so that building can hopefully begin towards the end of 2018.</p>
+
+			<p>We would love to hear from you if you have any suggestions for what to include in this exciting new project. To send us ideas, please see/ fill out the form below.</p>
 			
-				<h2>Other project</h2>
-				<div class="galleryprojects">
-				<!-- Gallery of the pictures, one large and three small-->
-				</div>
-				<div class="descprojects">
-				<!-- Description of the project-->
-				</div>
+			<!--FORM -->
+			<!--Name:
+			Email:
+			Suggestion:
+			Type message here
+			Attach a picture:
+			Upload
+			SUBMIT-->
+
+			
+			<h3>Two new rose species arriving</h3>
+			<p>Insert picture here
+			The Friends of the Botanical Garden are re-organising the rose beds to include two new species of roses. Due to growing interest in their work, and an increase in the number of group members, they are now able to expand the rose collection. Work will begin on this project early next month, and we can’t wait to see the results.
+
+			To see more about the work that the Friends of the Botanical Garden do for the park, visit their official Facebook page.</p>
+
+
+			<h3>Parking lot resurfacing</h3>
+			<p>Insert picture here
+			The main car park on Peter Holms Vej is in need of resurfacing, which will begin in two weeks’ time. In order to minimise the inconvenience during this time, the Kommune has secured free overflow parking in some of the surrounding roads. For a full list of where you can park, please see this map. 
+			The project should be completed within a month, however this is very weather dependent, so we will be making regular updates as the work progresses.</p>
+
+
+			<p>To get involved or for more information about any of these projects, feel free to contact us at [insert contact info]
+
+			* Maybe a separate email address for projects??
+
+			</p>
 				
 		</article>
 		<!--Footer:-->
