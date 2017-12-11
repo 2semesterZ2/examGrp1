78f4fb03daca45b54ec45f8634b7862fc8b23143
Petra Jakubcova <petrajakubcova@hotmail.com>
2017-12-11 15:58:22 +0100

    Adding pictures

--- projects.php
+++ projects.php
@@ -20,7 +20,8 @@
 			<h2>Upcoming projects</h2>
 
 			<h3>New Playground in the Botanical Park</h3>
-			<p>(Insert picture here)
+			<img src="/pictures/projectplayground.jpg" alt="Project playground">
+			<p>
 			We are excited to announce that a new all-inclusive playground will be coming soon to the Botanical Park! With a variety of different structures and sensory experiences, the playground will be a fun place for children of all ages to interact with each other out in nature.
 			We want this to be a place that brings the community together, and where better to do this than in the heart of the Botanical Park. Meetings are already underway about getting this community-lead project started, so that building can hopefully begin towards the end of 2018.</p>
 
@@ -37,6 +38,7 @@
 
 			
 			<h3>Two new rose species arriving</h3>
+			<img src="/pictures/roses.jpg" alt="roses">
 			<p>Insert picture here
 			The Friends of the Botanical Garden are re-organising the rose beds to include two new species of roses. Due to growing interest in their work, and an increase in the number of group members, they are now able to expand the rose collection. Work will begin on this project early next month, and we can’t wait to see the results.
 
@@ -44,6 +46,7 @@
 
 
 			<h3>Parking lot resurfacing</h3>
+			<img src="pictures/Car park resurfacing.jpg" alt="Car park">
 			<p>Insert picture here
 			The main car park on Peter Holms Vej is in need of resurfacing, which will begin in two weeks’ time. In order to minimise the inconvenience during this time, the Kommune has secured free overflow parking in some of the surrounding roads. For a full list of where you can park, please see this map. 
 			The project should be completed within a month, however this is very weather dependent, so we will be making regular updates as the work progresses.</p>
