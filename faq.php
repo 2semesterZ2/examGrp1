83d0179f8c970c2ac178f3a0ff8b985943d0e32c
UnkrautFee <kraftsa60869@th-nuernberg.de>
2017-12-12 22:01:13 +0100

    Delete uneccessary merge info
    from reverting the commit and merging it there were some lines added that I just deleted

--- faq.php
+++ faq.php
@@ -4,87 +4,81 @@ $description="Frequently asked questions about the The Botanical Park.";
 $keywords="botanical, park, aarhus, green, nature, denmark, city center, faq, questions, answers, did you know, interesting, facts ";
 include("includes/head.php"); ?>
 
-	<body>
-		<!--Header:-->
-		<?php include("includes/header.php"); ?>
-		<!--Navigation:-->
-		<?php include("includes/navigation.php"); ?>
+<body>
+<!--Header:-->
+<?php include("includes/header.php"); ?>
+<!--Navigation:-->
+<?php include("includes/navigation.php"); ?>
 
-		<!--Content here-->
-		<article>
-			<h1>Frequently Askes Questions</h1>
-<<<<<<< HEAD
-
-			<!--FAQ and their answers will be placed here-->
-
-=======
+<!--Content here-->
+	<article>
+		<h1>Frequently Askes Questions</h1>
             
-            <h2>General questions:</h2>
+            	<h2>General questions:</h2>
 
-            <div>
-                <p>What are the opening hours of the park?</p>
-                <div>The park is always open; there aren’t any specific opening hours in the park. You can always visit it, not matter daytime or nighttime. </div>
-                <p>How much does the entrance cost?</p>
-                <div>You don’t need to pay to enter to the park. It is for free, which makes it accessible for everyone.</div>
-                <p>How do I get to the Botanical Park?</p>
-                <div>There are different ways of transport to get to the park. You can find more detailed information on our <a href="location.php">location page</a>.</div>
-                <p>Are there any parking spaces? Do I have to pay for them?</p>
-                <div>There are different parking spaces around the area, some of them are for free. To see the exact position of them, visit our <a href="location.php">location page</a>.</div>
-                <p>Is the Botanical Park accessible for people with disabilities?</p>
-                <div>The park has different kinds of paths and can be steep in some places. But in general the Botanical Park is accessible for people with disabilities and they are more than welcome here. </div>
-                <p>What exact space does the Botanical Park occupy?</p>
-                <div>The Botanical Garden in total, including the 5 hectars Den Gamle By is placed on, covers 21.5 hectares. If you want to get further information about the different park areas and what is where, please visit our <a href="visitorinfo.php">visitor information</a> page.</div>
-                <p>What belongs to the Botanical Park?</p>
-                <div>The Tropical Houses and the Herb Garden belong to the Aarhus University. The volunteers are in charge of the Rose Garden. The Botanical Park is the rest of the space in the Botanical Garden. 
-                You can also have a look at our MAP and the information about the <a href="history.php">history</a> of the park.</div>
-                <p>Who are the Volunteers? Can I volunteer at the Botanical Park?</p>
-                <div>The Volunteers, also called the Friends of the Botanical Garden or Botanisk Haves Venner, are a group of volunteer workers who take care of certain areas in the park, like the Rose Garden. You can read more about the <a href="history.php">history</a> of the park on our website or you can get in contact with the Friends of the Botanical Garden themselves (PROVIDE EXTERNAL LINK TO THEIR FB OR SMTH). 
-                If you want to volunteer yourself in the park either contact US or the Friends of the Botanical Garden to get more information.</div>
-                <p>Is it possible to make a donation of plants?</p>
-                <div>If you plan on making a donation for the park, please <a href="contact.php">contact</a> us beforehand. Thank you! </div>
-                <p>Is it permitted to pick up seeds that have fallen to the ground, leaves or plants from the Botanical Park?</p>
-                <div>Fallen down leaves and seeds are alright to pick up, but please be careful to not step on other plants in the process. The plants are not allowed to be rooted out.</div>
-            </div>
+		    <div>
+			<p>What are the opening hours of the park?</p>
+			<div>The park is always open; there aren’t any specific opening hours in the park. You can always visit it, not matter daytime or nighttime. </div>
+			<p>How much does the entrance cost?</p>
+			<div>You don’t need to pay to enter to the park. It is for free, which makes it accessible for everyone.</div>
+			<p>How do I get to the Botanical Park?</p>
+			<div>There are different ways of transport to get to the park. You can find more detailed information on our <a href="location.php">location page</a>.</div>
+			<p>Are there any parking spaces? Do I have to pay for them?</p>
+			<div>There are different parking spaces around the area, some of them are for free. To see the exact position of them, visit our <a href="location.php">location page</a>.</div>
+			<p>Is the Botanical Park accessible for people with disabilities?</p>
+			<div>The park has different kinds of paths and can be steep in some places. But in general the Botanical Park is accessible for people with disabilities and they are more than welcome here. </div>
+			<p>What exact space does the Botanical Park occupy?</p>
+			<div>The Botanical Garden in total, including the 5 hectars Den Gamle By is placed on, covers 21.5 hectares. If you want to get further information about the different park areas and what is where, please visit our <a href="visitorinfo.php">visitor information</a> page.</div>
+			<p>What belongs to the Botanical Park?</p>
+			<div>The Tropical Houses and the Herb Garden belong to the Aarhus University. The volunteers are in charge of the Rose Garden. The Botanical Park is the rest of the space in the Botanical Garden. 
+			You can also have a look at our MAP and the information about the <a href="history.php">history</a> of the park.</div>
+			<p>Who are the Volunteers? Can I volunteer at the Botanical Park?</p>
+			<div>The Volunteers, also called the Friends of the Botanical Garden or Botanisk Haves Venner, are a group of volunteer workers who take care of certain areas in the park, like the Rose Garden. You can read more about the <a href="history.php">history</a> of the park on our website or you can get in contact with the Friends of the Botanical Garden themselves (PROVIDE EXTERNAL LINK TO THEIR FB OR SMTH). 
+			If you want to volunteer yourself in the park either contact US or the Friends of the Botanical Garden to get more information.</div>
+			<p>Is it possible to make a donation of plants?</p>
+			<div>If you plan on making a donation for the park, please <a href="contact.php">contact</a> us beforehand. Thank you! </div>
+			<p>Is it permitted to pick up seeds that have fallen to the ground, leaves or plants from the Botanical Park?</p>
+			<div>Fallen down leaves and seeds are alright to pick up, but please be careful to not step on other plants in the process. The plants are not allowed to be rooted out.</div>
+		    </div>
 
-            <h2>Activities in the Botanical Park:</h2>
+		    <h2>Activities in the Botanical Park:</h2>
 
-            <div>
-                <p>Can I take pictures in the park? </p>
-                <div>Yes, you are allowed to bring your camera and make some nice pictures in the park.</div>
-                <p>Am I allowed to use my dron in the park?</p>
-                <div> Please <a href="contact.php">contact</a> us to get more information about the usage of the drons in the park.</div>
-                <p> Can I jog at the park? What are the ideal places for running in the park?</p>
-                <div>The park is an ideal place to jog in. You can either use the paths, which are connecting different areas of the park, or get some more exercise on the grass areas. </div>
-                <p>Can I swim in a lake/river?</p>
-                <div> Unfortunately, it is not allowed to swim in the lake, since it is occupied by ducks and other animals, which we do not want to be disturbed.</div>
-                <p>Am I allowed to cycle/ ride a bike in the park?</p>
-                <div>The park is a nice place to walk on. Our visitors like to use the park spaces for walking and enjoying their time in the nature. Therefore, for the safety reasons it is not allowed to bike through the park.</div>
-                <p> Can I walk my dog in the park? </p>
-                <div>Yes, you are welcome to visit our park with your pet but you need to remember that it’s also necessary to use a leash for security reasons.</div>
-                <p> Are there any dog bins in the park?</p>
-                <div>No, there aren’t any dog bins in the park. You need to bring a plastic bag and use it to place the waste into the regular bins.</div>
-                <p>Am I allowed to have a Picnic/Barbecue in the park? </p>
-                <div>Yes, you are welcome to use the park’s spaces for having a picnic or barbecue. There aren’t any public grills in the park so we recommend you to bring a disposable one and throw it out to the special beans for it, located in the different areas of the park.</div>
-                <p>Can I make fire in the park? (A bonfire, a campfire…)</p>
-                <div>Because of the safety reasons, it is not allowed to make the bonfires inside of the park.</div>
-                <p>Is it possible to participate in community projects in partnership with the Botanical Park?</p>
-                <div>Yes it it. We recommend you to <a href="contact.php">contact</a> us to get more detailed information.</div>
-                <p>Do you offer an advice service for gardening? </p>
-                <div>No, not at the moment. But check our <a href="events.php">events</a> regularly, we might add some information about sessions in the future. Also you can <a href="contact.php">contact</a> us if you yourself want to offer workshops about that topic.</div>
-            </div>
-            
-            <h2>Events in the Botanical Park:</h2>
+		    <div>
+			<p>Can I take pictures in the park? </p>
+			<div>Yes, you are allowed to bring your camera and make some nice pictures in the park.</div>
+			<p>Am I allowed to use my dron in the park?</p>
+			<div> Please <a href="contact.php">contact</a> us to get more information about the usage of the drons in the park.</div>
+			<p> Can I jog at the park? What are the ideal places for running in the park?</p>
+			<div>The park is an ideal place to jog in. You can either use the paths, which are connecting different areas of the park, or get some more exercise on the grass areas. </div>
+			<p>Can I swim in a lake/river?</p>
+			<div> Unfortunately, it is not allowed to swim in the lake, since it is occupied by ducks and other animals, which we do not want to be disturbed.</div>
+			<p>Am I allowed to cycle/ ride a bike in the park?</p>
+			<div>The park is a nice place to walk on. Our visitors like to use the park spaces for walking and enjoying their time in the nature. Therefore, for the safety reasons it is not allowed to bike through the park.</div>
+			<p> Can I walk my dog in the park? </p>
+			<div>Yes, you are welcome to visit our park with your pet but you need to remember that it’s also necessary to use a leash for security reasons.</div>
+			<p> Are there any dog bins in the park?</p>
+			<div>No, there aren’t any dog bins in the park. You need to bring a plastic bag and use it to place the waste into the regular bins.</div>
+			<p>Am I allowed to have a Picnic/Barbecue in the park? </p>
+			<div>Yes, you are welcome to use the park’s spaces for having a picnic or barbecue. There aren’t any public grills in the park so we recommend you to bring a disposable one and throw it out to the special beans for it, located in the different areas of the park.</div>
+			<p>Can I make fire in the park? (A bonfire, a campfire…)</p>
+			<div>Because of the safety reasons, it is not allowed to make the bonfires inside of the park.</div>
+			<p>Is it possible to participate in community projects in partnership with the Botanical Park?</p>
+			<div>Yes it it. We recommend you to <a href="contact.php">contact</a> us to get more detailed information.</div>
+			<p>Do you offer an advice service for gardening? </p>
+			<div>No, not at the moment. But check our <a href="events.php">events</a> regularly, we might add some information about sessions in the future. Also you can <a href="contact.php">contact</a> us if you yourself want to offer workshops about that topic.</div>
+		    </div>
 
-            <div>
-                <p>Are there any events going on in the park?</p>
-                <div>Yes, we have some events during the year. Just have a look at our <a href="events.php">events</a> page to get a quick overview and more information. </div>
-                <p>Can I rent a space in the Botanical Park?</p>
-                <div>Yes, it is possible to rent the space. If it is for more than 30 people, you need to apply for it through (LINK :BrugAarhus.dk), which covers the department responsible for the use of public areas including the city parks.</div>
-                <p>Can I have my wedding in the Botanical Park?</p>
-                <div>It is not allowed to have a wedding reception in the park but you are welcome to come and make your wedding picture in the park.</div>
-            </div>
+		    <h2>Events in the Botanical Park:</h2>
+
+		    <div>
+			<p>Are there any events going on in the park?</p>
+			<div>Yes, we have some events during the year. Just have a look at our <a href="events.php">events</a> page to get a quick overview and more information. </div>
+			<p>Can I rent a space in the Botanical Park?</p>
+			<div>Yes, it is possible to rent the space. If it is for more than 30 people, you need to apply for it through (LINK :BrugAarhus.dk), which covers the department responsible for the use of public areas including the city parks.</div>
+			<p>Can I have my wedding in the Botanical Park?</p>
+			<div>It is not allowed to have a wedding reception in the park but you are welcome to come and make your wedding picture in the park.</div>
+		    </div>
 			
->>>>>>> parent of 5707155... Creating footer.css
 		</article>
 		<!--Footer:-->
 		<?php include("includes/footer.php"); ?>
