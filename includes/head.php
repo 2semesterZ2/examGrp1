1d4fcf9c36be5ca9bc90b73c254aa9844ecc2f7a
thecheesewolf <vikkto@gmail.com>
2017-12-12 13:04:43 +0100

    Adding google fonts
    Viktor didn't fuck up

--- includes/head.php
+++ includes/head.php
@@ -3,7 +3,13 @@
 
 <head>
 	<meta charset="UTF-8">
-	<title>Botanical Park</title>
+	<title>
+<?php echo $title; ?>
+	</title>
+	<!-- SEO keywords and description here -->
+	<meta name="description" content="<?php echo $description; ?>">
+		<meta name="keywords" content="<?php echo $keywords; ?>">
 	<!--CSS here-->
+<link href="https://fonts.googleapis.com/css?family=Open+Sans|Quicksand:400,500" rel="stylesheet">
 	<link rel="stylesheet" href="css/style.css" type="text/css">
 </head>
--- index.php
+++ index.php
@@ -1,4 +1,11 @@
-<?php include("includes/head.php"); ?>
+	<?php
+$title= "The Botanical Park | Home";
+$description="The Botanical Park is a beatiful green space, in the heart of Aarhus. It is free and open to everyone. Whether you like walking, jogging or just getting some fresh air, it's the perfect place to be out in nature.";
+$keywords="botanical, park, aarhus, green, nature, denmark, city center, trees, ducks, walking, activities, outdoors";
+	 include("includes/head.php");
+
+	 ?>
+
 
 <body>
 	<!--Header:-->
@@ -17,17 +24,13 @@
 	<div id="projectsandeventsbox">
 		<!--Projects and Events slideshow here-->
 	</div>
-	
+
 	<!--Clickable display of upcoming/ ongoing projects and events-->
 
 	<!--Weather box-->
 	<div id="weatherbox">
 		<h2>Today's Weather</h2>
-		<!--Daily weather from meteoblue.com --> 
-        <!--https://www.meteoblue.com/en/weather/forecast/week/aarhus_denmark_2624652?utm_source=weather_widget&utm_medium=linkus&utm_content=three&utm_campaign=Weather%2BWidget 
-        -->
-       <iframe src="https://www.meteoblue.com/en/weather/widget/three/aarhus_denmark_2624652?geoloc=fixed&nocurrent=0&noforecast=0&days=4&tempunit=CELSIUS&windunit=KILOMETER_PER_HOUR&layout=image"  frameborder="0" scrolling="NO" allowtransparency="true" sandbox="allow-same-origin allow-scripts allow-popups" style="width: 460px;height: 597px"></iframe>
-        
+		<!--Insert daily weather here-->
 	</div>
 
 	<!--Maintenance box-->
@@ -36,7 +39,7 @@
 		<p>There is no maintenance being carried out today :)<br/> We are mowing the lawns
 			<!--insert area description from 10am-->.<br/> The ponds are being cleaned over the next week, starting on [insert date].<br/> A sick tree is being removed from [insert area description] today.</p>
 		<!--Maintenance log link -->
-		
+
 	</div>
 	</article>
 	<!--Footer:-->
