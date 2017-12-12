5707155e6d2b20e10ac4d9bb2f3d0cd68a5ad92d
thecheesewolf <vikkto@gmail.com>
2017-12-12 18:37:47 +0100

    Creating footer.css
    I've tried to make the footer look nice to a human eye.
    Changed the kommune logo
    Added some extra links.
    Didn't touch anything else I promise

--- includes/footer.php
+++ includes/footer.php
@@ -1,22 +1,33 @@
+<head>
+	<link rel="stylesheet" href="css/footer.css" type="text/css">
+</head>
+
 <footer>
-	
-	<div class="contactInformation">
+<div class="footer-content">
+	<ul>
+
+		</ul>
+		<div class="logo-copyright">
+		<img src="pictures/kommune-logo.png" alt="Aarhus Kommune's logo" class="footerLogo"/>
+		<li id="last"><small>&copy; Copyright Aarhus Kommune 2017</small> </li>
+		</div>
+<div class="contactInformation">
 		<h3>Contact</h3>
-		<p>Aarhus Kommune<br />
-		Adress: Hack Kampmanns Plads 2<br />
-		8000 Aarhus C<br />
-		<br />
-		&#128222 : 89 40 44 00 
-		</p>
+		<li>Aarhus Kommune</li>
+		<li>Adress: Hack Kampmanns Plads 2</li>
+		<li>8000 Aarhus C</li>
+		<br/>
+		<li>& #12822 : 89 40 44 00</li>
 	</div>
 	<div class="socialMediaInformation">
 		<!--link to social media-->
+		<a href="https://www.facebook.com/AarhusKommune/">Facebook</a>
 	</div>
-	<div class="blogInformation">
+	<div class="links">
 		<!--link to our blog-->
+<li><a href="faq.php">FAQ</a></li>
+<li><a href="blog.php">Blog</a></li>
 	</div>
-	
-	<img src="pictures/aarhus-kommune-logo.jpg" alt="Aarhus Kommune's logo" class="footerLogo" 
-	<p id="last"><small>&copy; Copyright Aarhus Kommune 2017</small> </p>
-	
+	</div>
+
 </footer>
