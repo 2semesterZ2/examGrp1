693256b78cdd5eff5d091ba6a93214125d83f15a
sarahberglund <sarah.keen15@gmail.com>
2017-12-13 09:54:50 +0100

    Initial styling of the home page
    Background colour and article colour added across all pages, positioning of the boxes with float, added image as a placeholder for slideshow,

--- index.php
+++ index.php
@@ -23,6 +23,7 @@
 		<h2>See what is happening in the park</h2>
 		<div id="projectsandeventsbox">
 			<!--Projects and Events slideshow here-->
+			<img src="pictures/projectplayground.jpg" alt="Picture of a playground in a park">
 		</div>
 
 		<!--Clickable display of upcoming/ ongoing projects and events-->
@@ -32,10 +33,9 @@
 			<h2>Today's Weather</h2>
 			<!--Insert daily weather here-->
 			<!--Daily weather from meteoblue.com -->
-			-
 			<!--https://www.meteoblue.com/en/weather/forecast/week/aarhus_denmark_2624652?utm_source=weather_widget&utm_medium=linkus&utm_content=three&utm_campaign=Weather%2BWidget
  -        -->
-			- <iframe src="https://www.meteoblue.com/en/weather/widget/three/aarhus_denmark_2624652?geoloc=fixed&nocurrent=0&noforecast=0&days=4&tempunit=CELSIUS&windunit=KILOMETER_PER_HOUR&layout=image" frameborder="0" scrolling="NO" allowtransparency="true" sandbox="allow-same-origin allow-scripts allow-popups" style="width: 460px;height: 597px"></iframe>
+			<iframe src="https://www.meteoblue.com/en/weather/widget/three/aarhus_denmark_2624652?geoloc=fixed&nocurrent=0&noforecast=0&days=4&tempunit=CELSIUS&windunit=KILOMETER_PER_HOUR&layout=image" frameborder="0" scrolling="NO" allowtransparency="true" sandbox="allow-same-origin allow-scripts allow-popups" style="width: 460px;height: 597px"></iframe>
 		</div>
 
 		<!--Maintenance box-->
@@ -51,5 +51,4 @@
 	<?php include("includes/footer.php"); ?>
 </body>
 
-	<
-	/ html>
+	</html>
