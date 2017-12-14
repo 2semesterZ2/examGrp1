4f5d07d33548ddaa6ce3ed39cbd49d8014442451
Petra Jakubcova <petrajakubcova@hotmail.com>
2017-12-14 14:56:41 +0100

    working on History

--- history.php
+++ history.php
@@ -14,20 +14,11 @@ $keywords="botanical, park, aarhus, green, nature, denmark, city center, trees, 
 		<!--Short history section-->
 		<article>
 		<h1>Short history of the Botanical Garden</h1>
+		<div id="historyTextLeft">
 		<p>The Botanical Garden was established as an experimental green area for fruit trees and other useful plants in 1873. By 1875 the area grew to 4.5 hectares, and was part of the ‘Møllehavnen Toft’. This was just east of Hasleforten, where a newly established plant company the ‘Haveselskab’ constructed a nursery and expanded the area. The Haveselskab was later renamed to the ‘Jydske Haveselskab’.</p>
-
-		<!--Old map here-->
-		<div id="oldMap">
-			<img src="pictures/old_map2.jpg" alt="Old map of the Botanical Park">
-		</div>
-		
-		<p>During the following years, the company rented more land, and the garden grew to approximately 8 hectares. By 1903 a roadmap for future improvements was created, which included plans to build the botanical garden.
-		Jydske Haveselskab was struggling to cover the maintenance costs, so by 1911 the Kommune overtook the area, and approved the plans to establish it as a garden.
-		The garden grew gradually, and by the end of 1946 it reached 21.5 hectares in size, which is how it stands today.</p>
-
-		<p>2012 was a big change in the Garden’s life. Following budget negotiations, the Greenhouses were overtaken by the Science museum, and a collaboration started with the Friends of the Botanical Gardens who take care of the Rose Gardens. The volunteers formed to keep elements of the botanical past alive within the park, as this was nearly lost completely.</p>
-       
-        <!--Gallery of the pictures on the side-->
+	    <br>
+	    <br>
+		    <!--Gallery of the pictures on the side-->
         <div class="oldGallery">
 		<div class="largePicture" id="picture1">
 			<img src="pictures/oldConcert.jpg" alt="Old picture of the concert in the park">
@@ -45,6 +36,19 @@ $keywords="botanical, park, aarhus, green, nature, denmark, city center, trees, 
 			<img src="pictures/oldViewLake.jpg" alt="Old picture of the lake">
 		</div>
 		</div>
+
+		<!--Old map here-->
+		<!-- id="oldMap">
+			<img src="pictures/old_map2.jpg" alt="Old map of the Botanical Park">
+		</div>-->
+		
+		<!--Text to left-->
+		<p>During the following years, the company rented more land, and the garden grew to approximately 8 hectares. By 1903 a roadmap for future improvements was created, which included plans to build the botanical garden.
+		Jydske Haveselskab was struggling to cover the maintenance costs, so by 1911 the Kommune overtook the area, and approved the plans to establish it as a garden.
+		The garden grew gradually, and by the end of 1946 it reached 21.5 hectares in size, which is how it stands today.</p>
+			</div>
+		
+		<p>2012 was a big change in the Garden’s life. Following budget negotiations, the Greenhouses were overtaken by the Science museum, and a collaboration started with the Friends of the Botanical Gardens who take care of the Rose Gardens. The volunteers formed to keep elements of the botanical past alive within the park, as this was nearly lost completely.</p>
 		
 		<!--Interactive map here-->
 		<div id="newMap">
