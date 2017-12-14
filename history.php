d400d1089ab6f3d17732ee3931a11904173586e6
Petra Jakubcova <petrajakubcova@hotmail.com>
2017-12-14 16:59:11 +0100

    History subpage
    boxing

--- history.php
+++ history.php
@@ -13,6 +13,7 @@ $keywords="botanical, park, aarhus, green, nature, denmark, city center, trees, 
 		<!--Content here-->
 		<!--Short history section-->
 		<article>
+		
 		<h1>Short history of the Botanical Garden</h1>
 		<div id="historyTextLeft">
 		<p>The Botanical Garden was established as an experimental green area for fruit trees and other useful plants in 1873. By 1875 the area grew to 4.5 hectares, and was part of the ‘Møllehavnen Toft’. This was just east of Hasleforten, where a newly established plant company the ‘Haveselskab’ constructed a nursery and expanded the area. The Haveselskab was later renamed to the ‘Jydske Haveselskab’.</p>
@@ -43,23 +44,33 @@ $keywords="botanical, park, aarhus, green, nature, denmark, city center, trees, 
 		</div>-->
 		
 		<!--Text to left-->
+		<div class="textLeft">
 		<p>During the following years, the company rented more land, and the garden grew to approximately 8 hectares. By 1903 a roadmap for future improvements was created, which included plans to build the botanical garden.
 		Jydske Haveselskab was struggling to cover the maintenance costs, so by 1911 the Kommune overtook the area, and approved the plans to establish it as a garden.
 		The garden grew gradually, and by the end of 1946 it reached 21.5 hectares in size, which is how it stands today.</p>
-			</div>
 		
 		<p>2012 was a big change in the Garden’s life. Following budget negotiations, the Greenhouses were overtaken by the Science museum, and a collaboration started with the Friends of the Botanical Gardens who take care of the Rose Gardens. The volunteers formed to keep elements of the botanical past alive within the park, as this was nearly lost completely.</p>
-		
-		<!--Interactive map here-->
-		<div id="newMap">
-			<img src="pictures/areas-map.jpg" alt="Map of the Botanical Park and its areas">
 		</div>
+		<!--Interactive map here-->
+		<!-- id="newMap">
+			<img src="pictures/areas-map.jpg" alt="Map of the Botanical Park and its areas">
+		</div>-->
+	
+		<div class="maps">
+		<div class="newMap">
+			<img src="pictures/areas-mapphotoshop.jpg" alt="New map">
+		</div>
+		<div class="oldMap">
+			<img src="pictures/old_map2.jpg" alt="Old map">
+		</div>
+		</div>
+		<!--Gallery containing the old pictures here?-->
+		<div class="textRight">
 		<p>Now the land of the Botanical Garden contains 3 main areas:
 		Den Gamle By, which takes up 5 hectares of the Garden and is on of the top tourist attraction in Aarhus official website
 		The Greenhouses, operated by the Science museum official website
 		And the Botanical Park, which is looked after by the Kommune official website and the Friends of the Botanical Garden Facebook group </p>
-
-		<!--Gallery containing the old pictures here?-->
+		</div>
 
 		</article>
 		<!--Footer:-->
