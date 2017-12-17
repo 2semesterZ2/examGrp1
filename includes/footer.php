2793b077fa67dd036933a8d1016bb9d2027ba698
Unkraut.Fee <kraftsa60869@th-nuernberg.de>
2017-12-17 21:57:54 +0100

    adding the disclaimer

--- includes/footer.php
+++ includes/footer.php
@@ -1,29 +1,28 @@
 <footer>
 	<link rel="stylesheet" href="css/footer.css" type="text/css">
-<div class="footer-content">
-	<ul>
-
-		</ul>
-		<div class="logo-copyright">
-		<img src="pictures/kommune-logo.png" alt="Aarhus Kommune's logo" class="footerLogo"/>
-		<li id="last"><small>&copy; Copyright Aarhus Kommune 2017</small> </li>
+    <div class="footer-content">
+        <p>Disclaimer: this website is a study project created by students of the BAAA, and is not an official page of the Aarhus Kommune or the Botanical Garden, nor the Botanical Park. </p>
+        <br/>
+        <div class="logo-copyright">
+            <img src="pictures/kommune-logo.png" alt="Aarhus Kommune's logo" class="footerLogo"/>
+            <li id="last"><small>&copy; Copyright Aarhus Kommune 2017</small> </li>
 		</div>
-<div class="contactInformation">
-		<h3>Contact</h3>
-	<div id="contactlist">
-		<li>Aarhus Kommune</li>
-		<li>Address: Hack Kampmanns Plads 2</li>
-		<li>8000 Aarhus C</li>
-		<br/>
-		<li>&#128222 : 89 40 44 00</li>
-	</div>
-	</div>
-	<div class="links">
-		<!--link to social media-->
-		<a href="https://www.facebook.com/AarhusKommune/">Facebook</a>
-		<!--link to our blog-->
-<li><a href="faq.php">FAQ</a></li>
-	</div>
+        <div class="contactInformation">
+            <h3>Contact</h3>
+            <div id="contactlist">
+                <li>Aarhus Kommune</li>
+                <li>Address: Hack Kampmanns Plads 2</li>
+                <li>8000 Aarhus C</li>
+                <br/>
+                <li>&#128222 : 89 40 44 00</li>
+	       </div>
+        </div>
+        <div class="links">
+            <!--link to social media-->
+            <a href="https://www.facebook.com/AarhusKommune/">Facebook</a>
+            <!--link to our blog-->
+            <li><a href="faq.php">FAQ</a></li>
+        </div>
 	</div>
 
 </footer>
