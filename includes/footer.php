0d8d96e6dcbec675d96adc237b9b8a90915a5b95
sarahberglund <sarah.keen15@gmail.com>
2018-01-10 17:16:34 +0100

    Styling to scale footer for different screen width
    Media queries added for different screen widths for the footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,40 +1,31 @@
 <footer>
 	<link rel="stylesheet" href="css/footer.css" type="text/css">
-    <div class="footer-content">
-        <br/>
-        <div class="logo-copyright">
-            <img src="pictures/kommune-logo.png" alt="Aarhus Kommune's logo" class="footerLogo"/>
-            <p id="last"><small>&copy; Copyright Aarhus Kommune 2017</small> </p>
+	<div class="footer-content">
+		<br/>
+		<div class="contactInformation">
+			<h3>Contact</h3>
+			<div id="contactlist">
+				<p>Aarhus Kommune</p>
+				<p>Address: Hack Kampmanns Plads 2</p>
+				<p>8000 Aarhus C</p>
+				<br/>
+				<p>&#128222 : 89 40 44 00</p>
+			</div>
 		</div>
-        <div class="contactInformation">
-            <h3>Contact</h3>
-            <div id="contactlist">
-                <p>Aarhus Kommune</p>
-                <p>Address: Hack Kampmanns Plads 2</p>
-                <p>8000 Aarhus C</p>
-                <br/>
-                <p>&#128222 : 89 40 44 00</p>
-	       </div>
-        </div>
-        <div class="links">
-            <!--link to social media-->
-            <a href="https://www.facebook.com/AarhusKommune/">Facebook</a>
-            <!--link to our blog-->
-            <p><a href="faq.php">FAQ</a></p>
-        </div>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <p id="disclaimer">Disclaimer: this website is a study project created by students of the BAAA, and is not an official page of the Aarhus Kommune or the Botanical Garden, nor the Botanical Park. </p>
-        <br/>
-        <br/>
-	</div>
+		<div class="links">
+			<!--link to social media-->
+			<a href="https://www.facebook.com/AarhusKommune/">Facebook</a>
+			<!--link to our blog-->
+			<p><a href="faq.php">FAQ</a>
+			</p>
+		</div>
+		<div class="logo-copyright">
+			<img src="pictures/kommune-logo.png" alt="Aarhus Kommune's logo" class="footerLogo"/>
+			<p id="last"><small>&copy; Copyright Aarhus Kommune 2017</small> </p>
+		</div>
+		<br/>
 
+		<p id="disclaimer">Disclaimer: this website is a study project created by students of the BAAA, and is not an official page of the Aarhus Kommune or the Botanical Garden, nor the Botanical Park. </p>
+		<br/>
+	</div>
 </footer>
